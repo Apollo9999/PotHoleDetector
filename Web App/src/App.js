c1d61aaa0bc0e5c0c002731ea050d60c1ff32bb7
vjainlion <44116426+vjainlion@users.noreply.github.com>
2020-06-21 04:01:51 +0530

    Update app.js

--- Web App/src/App.js
+++ Web App/src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Header from './component/header/header.component.jsx';
 import Report from './component/report/report.component.jsx';
 import './App.css'
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route ,Redirect} from 'react-router-dom';
 import {BrowserRouter as Router} from 'react-router-dom'; 
 import SignInAndSignUpPage from './component/SignInAndSignUpPage/SignInAndSignUpPage.component.jsx';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 class App extends React.Component{
 
 
@@ -13,8 +14,10 @@ class App extends React.Component{
       super();
       this.state = 
       {
+          currentUser:null,
           name:'',
-          location:''
+          location:'',
+          imageUrl:''
 
       };
   }
@@ -24,16 +27,63 @@ class App extends React.Component{
     this.setState({[e.target.name]:e.target.value});
     console.log(this.state);
   }
+  handleUploading = (state)=>
+  {
+    const imageUrl = state.url;
+    this.setState({imageUrl});
+    console.log(this.state);
+  }
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      createUserProfileDocument(userAuth);
+       this.setState({currentUser:userAuth});
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state.currentUser);
+        });
+      }
+      else{
+        this.setState({ currentUser: userAuth });
+      }
+      console.log(this.state.currentUser);
+     
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
 
    render(){
     return (
       <div className="App">
-     <Header/>
-     <Router>
+     <Header currentUser={this.state.currentUser}/>
+    
+     <switch>
+     <Route exact path='/' render={(props)=> <Report {...props} state={this.state} handleChange={this.handleChange} handleUploading={this.handleUploading}/>}/>
+     <Route
+     exact
+     path='/signin'
+     render={() =>
+       this.state.currentUser ? (
+         <Redirect to='/' />
+       ) : (
+         <SignInAndSignUpPage />
+       )
+     }
+   />
+     </switch>
+
      
-   <Route exact path='/' component={Report}/>
-     <Route path='/signin' component={SignInAndSignUpPage}/>
-     </Router>
       </div>
     );
 
